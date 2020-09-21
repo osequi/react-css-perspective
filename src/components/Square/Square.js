9246d2d125333a20788dd675652cf5a67cf27f8d
osequi <cs@osequi.com>
2020-09-21 10:06:15 +0300

    Anime.js works like a charm. Replacing Framer Motion

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
-import { motion } from "framer-motion";
+
+import anime from "animejs";
 
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
@@ -59,7 +60,7 @@ const Square = (props) => {
   /**
    * Manages states and state changes
    */
-  const [axis, setAxis] = useState("axisX");
+  const [axis, setAxis] = useState("X");
   const [perspective, setPerspective] = useState(false);
 
   const handleAxisChange = (event) => {
@@ -73,25 +74,47 @@ const Square = (props) => {
   /**
    * Defines the animations
    */
-  const transitionRepeat = {
-    duration: 2,
+  const move = (axis) => {
+    switch (axis) {
+      case "Z":
+        return [
+          { translateZ: 50 },
+          { translateZ: 0 },
+          { translateZ: -50 },
+          { translateZ: 0 },
+        ];
+      case "Y":
+        return [
+          { translateY: 50 },
+          { translateY: 0 },
+          { translateY: -50 },
+          { translateY: 0 },
+        ];
+      case "X":
+      default:
+        return [
+          { translateX: 50 },
+          { translateX: 0 },
+          { translateX: -50 },
+          { translateX: 0 },
+        ];
+    }
   };
 
-  const variantsForAxis = {
-    axisX: { translateX: ["-50px", "50px"] },
-    axisY: { translateY: ["-50px", "50px"] },
-    axisZ: { translateZ: ["-50px", "50px"] },
-  };
+  useEffect(() => {
+    anime({
+      targets: ".Square",
+      keyframes: move(axis),
+      loop: true,
+      duration: 2000,
+      easing: "linear",
+    });
+  }, [axis]);
 
   return (
     <div className={clsx("Container", container)}>
       <div className={clsx("SquareContainer", squareContainer)}>
-        <motion.div
-          className={clsx("Square", square)}
-          animate={axis}
-          variants={variantsForAxis}
-          transition={transitionRepeat}
-        />
+        <div className={clsx("Square", square)} />
       </div>
       <div className={clsx("Legend", legend)}>
         <FormControl component="fieldset">
@@ -102,21 +125,9 @@ const Square = (props) => {
             value={axis}
             onChange={handleAxisChange}
           >
-            <FormControlLabel
-              value="axisX"
-              control={<Radio />}
-              label="X axis"
-            />
-            <FormControlLabel
-              value="axisY"
-              control={<Radio />}
-              label="Y axis"
-            />
-            <FormControlLabel
-              value="axisZ"
-              control={<Radio />}
-              label="Z axis"
-            />
+            <FormControlLabel value="X" control={<Radio />} label="X axis" />
+            <FormControlLabel value="Y" control={<Radio />} label="Y axis" />
+            <FormControlLabel value="Z" control={<Radio />} label="Z axis" />
           </RadioGroup>
           <FormControlLabel
             control={
